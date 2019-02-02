f38716eb01d8c1c93304bc3c2138d68b53a28c72
PanStar <panc@chinaoly.com>
2019-02-02 17:23:31 +0800

    优化包的大小

--- src/index.js
+++ src/index.js
@@ -160,14 +160,14 @@ export function formatDate (date = new Date(), fmt = 'yyyy-MM-dd hh:mm:ss.S') {
   }
   for (var k in o) {
     if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
     }
   }
   return fmt;
 }
 
 export default {
-  version: '0.0.1',
+  version: '0.0.4',
   delay,
   arrayToMap,
   mapToArray,
